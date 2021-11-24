d82a2c5e2ed17824f765323ebb927aebc16c070d
aeksei <aeksei94@gmail.com>
2021-11-24 20:44:47 +0300

    fix strip on rstrip

--- Занятие3/Практические_задания/task1_1/main.py
+++ Занятие3/Практические_задания/task1_1/main.py
@@ -2,7 +2,7 @@ def task():
     filename = "input.txt"
     with open(filename) as f:  # менеджер контекста открывает файл в режиме чтения в текстовом формате "rt"
         for line in f:  # файл является итератором, который построчно возвращает свое содержимое
-            line = line.strip()
+            line = line.rstrip()
             print(line)
 
 
