301497977fd5d810338ee96cca4f1fc1a212c53f
aeksei <aeksei94@gmail.com>
2022-11-09 12:34:27 +0300

    update task9 in lab3

--- Тема 3/Лабораторные работы/task9/main.py
+++ Тема 3/Лабораторные работы/task9/main.py
@@ -3,8 +3,8 @@ spend = 6000  # траты
 months = 10  # количество месяцев
 increase = 0.03  # рост цен
 
-need_money = 0  # количество денег, чтобы прожить 10 месяцев
+money_capital = 0  # количество денег, чтобы прожить 10 месяцев
 
 # TODO Оформить решение
 
-print(round(need_money))
+print(round(money_capital))
