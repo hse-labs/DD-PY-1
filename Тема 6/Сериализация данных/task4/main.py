83216770921e4b0ff68e048f858e69b5febd47d3
aeksei <aeksei94@gmail.com>
2022-11-09 16:06:57 +0300

    add encoding and delete output.json

--- Тема 6/Сериализация данных/task4/main.py
+++ Тема 6/Сериализация данных/task4/main.py
@@ -5,15 +5,15 @@ OUTPUT_FILE = "output.json"
 
 
 def task() -> None:
-    with open(INPUT_FILE) as f:
+    with open(INPUT_FILE, encoding="utf-8") as f:
         json_data = json.load(f)
 
-    with open(OUTPUT_FILE, "w") as f:
+    with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
         json.dump(json_data, f, indent=4, ensure_ascii=False)
 
 
 task()
 
-with open(OUTPUT_FILE) as output_f:
+with open(OUTPUT_FILE, encoding="utf-8") as output_f:
     for line in output_f:
         print(line, end="")
