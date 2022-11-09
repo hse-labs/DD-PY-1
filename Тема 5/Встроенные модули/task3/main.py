8bb8ffeec786bd05bbd0534426256e272970ca7f
aeksei <aeksei94@gmail.com>
2022-11-09 16:11:43 +0300

    update solve

--- Тема 5/Встроенные модули/task3/main.py
+++ Тема 5/Встроенные модули/task3/main.py
@@ -2,7 +2,7 @@ from random import randint
 
 start = -100
 stop = 100
-count = 20
+count = 50
 list_numbers = [randint(start, stop) for _ in range(count)]
 
 negative_numbers = [num for num in list_numbers if num < 0]
